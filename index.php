230c9826088a368aa73d9f50ba0d5fd96a4f7b88
sgukz <thorapee055@gmail.com>
2018-08-06 08:32:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>ระบบแสดงคิวการใช้บริการโรงพยาบาลร้อยเอ็ด</title>
 </head>
 <body>
     <script langquage='javascript'>
-     url=document.location;
+     /*url=document.location;
      if ((url=="https://services-link.herokuapp.com/") || (url=="https://services-link.herokuapp.com")){
          document.write("<head><title>ระบบแสดงคิวการใช้บริการโรงพยาบาลร้อยเอ็ด</title></head>");
          document.write('<frameset cols="*">');
@@ -20,7 +20,7 @@
          document.write('<frameset cols="*">');
          document.write('<frame src="http://49.231.5.51/api-queue-reh/">');
          document.write("</frameset>");
-     }
+     }*/
     </script>
     <h1>Hi, My site</h1>
 </body>

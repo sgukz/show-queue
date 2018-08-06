9217c0aef0ddda3fb1b1d697c6d2b30ec5c61160
sgukz <thorapee055@gmail.com>
2018-08-06 08:26:40 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <script langquage='javascript'>
+    // url=document.location;
+    // if ((url=="https://services-link.herokuapp.com") || (url=="https://services-link.herokuapp.com/")){
+    //     document.write("<head><title>ระบบแสดงคิวการใช้บริการ</title></head>");
+    //     document.write('<frameset cols="*">');
+    //     document.write('<frame src="http://49.231.5.51/api-queue-reh/index.html">');
+    //     document.write("</frameset>");
+    // }
+    // // if((url=="http://www.reh.go.th") || (url=="http://www.reh.go.th/")){
+    // //     document.write("<head><title>โรงพยาบาลร้อยเอ็ดยินดีต้อนรับ-WEB2</title></head>");
+    // //     document.write('<frameset cols="*">');
+    // //     document.write('<frame src="http://49.231.5.51/api-queue-reh/index.html">');
+    // //     document.write("</frameset>");
+    // // }
+    </script>
+    <h1>Hi, My site</h1>
+</body>
+</html>
